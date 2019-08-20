60d4067f82150deac2b9f28813b543528a7f01e1
Mar Sánchez <mayela0x14@gmail.com>
2019-08-20 17:41:11 -0500

    Refactor function: use slice list instead of reversed

--- wordplay/is_palindrome.py
+++ wordplay/is_palindrome.py
@@ -3,7 +3,7 @@ import scrabble
 longest = ""
 
 for word in scrabble.wordlist:
-    if list(word) == list(reversed(word)) and len(word) > len(longest):
+    if word == word[::-1] and len(word) > len(longest):
         longest = word
 
 print(longest)
