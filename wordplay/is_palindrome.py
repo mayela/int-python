6633fc068d9811b45b1795813c1f96df9d81057f
Mar Sánchez <mayela0x14@gmail.com>
2019-08-20 17:39:11 -0500

    Refactor function: use reversed instead of index manipulation

--- wordplay/is_palindrome.py
+++ wordplay/is_palindrome.py
@@ -3,10 +3,7 @@ import scrabble
 longest = ""
 
 for word in scrabble.wordlist:
-    is_palindrome = True
-    for index in range(len(word)):
-        if word[index] != word[-(index + 1)]:
-            is_palindrome = False
-    if is_palindrome and len(word) > len(longest):
+    if list(word) == list(reversed(word)) and len(word) > len(longest):
         longest = word
+
 print(longest)
