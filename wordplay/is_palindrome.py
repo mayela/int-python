5947a5a3027976c2654a765943fee1409e483320
Mar Sánchez <mayela0x14@gmail.com>
2019-08-20 17:32:42 -0500

    Add function to find the longest palindrome on wordlist

---
+++ wordplay/is_palindrome.py
@@ -0,0 +1,12 @@
+import scrabble
+
+longest = ""
+
+for word in scrabble.wordlist:
+    is_palindrome = True
+    for index in range(len(word)):
+        if word[index] != word[-(index + 1)]:
+            is_palindrome = False
+    if is_palindrome and len(word) > len(longest):
+        longest = word
+print(longest)
