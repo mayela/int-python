c516008582e207317c24bdd98153d86f3d8f42b4
Mar Sánchez <mmiranda@nearsoft.com>
2019-08-22 12:21:56 -0500

    Improve performance changing the list to a dict

--- wordplay/sonnets.py
+++ wordplay/sonnets.py
@@ -2,12 +2,13 @@ import time
 
 my_words = [elt.strip() for elt in open("sonnet_words.txt", "r").readlines()]
 word_list = [elt.strip() for elt in open("sowpods.txt", "r").readlines()]
+word_dict = dict((elt, 1) for elt in word_list)
 
 counter = 0
 
 start = time.time()
 for word in my_words:
-    if word not in word_list:
+    if word not in word_dict:
         print(word)
         counter += 1
 stop = time.time()
